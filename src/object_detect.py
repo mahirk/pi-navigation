7a6eff85e4bee0f7d709cdbe29ebc0ae9ff5081d
Mahir Kothary <m.kothary95@gmail.com>
2021-12-07 15:00:07 -0500

    add code

--- src/object_detect.py
+++ src/object_detect.py
@@ -1,10 +1,24 @@
+# Import packages
+import os
+import argparse
 import cv2
-import qwiic
 import numpy as np
+import sys
+import time
+from threading import Thread
+import importlib.util
+import qwiic
 from numpy import ones, vstack
 from numpy.linalg import lstsq
 import math
 from scipy.optimize import curve_fit
+import qwiic_button
+
+object_name, bounding_box = "", ()
+my_button = qwiic_button.QwiicButton()
+if my_button.begin() == False:
+        print("\nThe Qwiic Button isn't connected to the system. Please check your connection")
+
 
 cap = cv2.VideoCapture(0)
 
@@ -213,7 +227,10 @@ def run(screen):
 
     if distanceFeet < 0.7:
         distance_is_too_close = True
-        write_and_print("STOP: There is an object less than 1 foot away, please turn to continue navigation.")
+        get_objects()
+        cv2.rectangle(*bounding_box)
+
+        write_and_print("STOP: There is a {} less than 1 foot away, please turn to continue navigation.".format(object_name if object_name is not "" else "object"))
 
     line = cv2.HoughLinesP(fin, 2, np.pi / 180, 20, 7, 7)
     if not (line is None):
@@ -307,9 +324,214 @@ def run(screen):
 
     return screen
 
+
+# Define VideoStream class to handle streaming of video from webcam in separate processing thread
+# Source - Adrian Rosebrock, PyImageSearch: https://www.pyimagesearch.com/2015/12/28/increasing-raspberry-pi-fps-with-python-and-opencv/
+class VideoStream:
+    """Camera object that controls video streaming from the Picamera"""
+
+    def __init__(self, resolution=(640, 480), framerate=30):
+        # Initialize the PiCamera and the camera image stream
+        self.stream = cap
+        ret = self.stream.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
+        ret = self.stream.set(3, resolution[0])
+        ret = self.stream.set(4, resolution[1])
+
+        # Read first frame from the stream
+        (self.grabbed, self.frame) = self.stream.read()
+
+        # Variable to control when the camera is stopped
+        self.stopped = False
+
+    def start(self):
+        # Start the thread that reads frames from the video stream
+        Thread(target=self.update, args=()).start()
+        return self
+
+    def update(self):
+        # Keep looping indefinitely until the thread is stopped
+        while True:
+            # If the camera is stopped, stop the thread
+            if self.stopped:
+                # Close camera resources
+                self.stream.release()
+                return
+
+            # Otherwise, grab the next frame from the stream
+            (self.grabbed, self.frame) = self.stream.read()
+
+    def read(self):
+        # Return the most recent frame
+        return self.frame
+
+    def stop(self):
+        # Indicate that the camera and thread should be stopped
+        self.stopped = True
+
+
+# Define and parse input arguments
+### Copied from Author: Evan Juras
+parser = argparse.ArgumentParser()
+parser.add_argument('--modeldir', help='Folder the .tflite file is located in',
+                    required=True)
+parser.add_argument('--graph', help='Name of the .tflite file, if different than detect.tflite',
+                    default='detect.tflite')
+parser.add_argument('--labels', help='Name of the labelmap file, if different than labelmap.txt',
+                    default='labelmap.txt')
+parser.add_argument('--threshold', help='Minimum confidence threshold for displaying detected objects',
+                    default=0.5)
+parser.add_argument('--resolution',
+                    help='Desired webcam resolution in WxH. If the webcam does not support the resolution entered, errors may occur.',
+                    default='1280x720')
+parser.add_argument('--edgetpu', help='Use Coral Edge TPU Accelerator to speed up detection',
+                    action='store_true')
+
+args = parser.parse_args()
+
+MODEL_NAME = args.modeldir
+GRAPH_NAME = args.graph
+LABELMAP_NAME = args.labels
+min_conf_threshold = float(args.threshold)
+resW, resH = args.resolution.split('x')
+imW, imH = int(resW), int(resH)
+use_TPU = args.edgetpu
+
+# Import TensorFlow libraries
+# If tflite_runtime is installed, import interpreter from tflite_runtime, else import from regular tensorflow
+# If using Coral Edge TPU, import the load_delegate library
+### Copied from Author: Evan Juras
+
+pkg = importlib.util.find_spec('tflite_runtime')
+if pkg:
+    from tflite_runtime.interpreter import Interpreter
+
+    if use_TPU:
+        from tflite_runtime.interpreter import load_delegate
+else:
+    from tensorflow.lite.python.interpreter import Interpreter
+
+    if use_TPU:
+        from tensorflow.lite.python.interpreter import load_delegate
+
+# If using Edge TPU, assign filename for Edge TPU model
+if use_TPU:
+    # If user has specified the name of the .tflite file, use that name, otherwise use default 'edgetpu.tflite'
+    if (GRAPH_NAME == 'detect.tflite'):
+        GRAPH_NAME = 'edgetpu.tflite'
+
+    # Get path to current working directory
+CWD_PATH = os.getcwd()
+
+# Path to .tflite file, which contains the model that is used for object detection
+PATH_TO_CKPT = os.path.join(CWD_PATH, MODEL_NAME, GRAPH_NAME)
+
+# Path to label map file
+PATH_TO_LABELS = os.path.join(CWD_PATH, MODEL_NAME, LABELMAP_NAME)
+
+# Load the label map
+with open(PATH_TO_LABELS, 'r') as f:
+    labels = [line.strip() for line in f.readlines()]
+
+# Have to do a weird fix for label map if using the COCO "starter model" from
+# https://www.tensorflow.org/lite/models/object_detection/overview
+# First label is '???', which has to be removed.
+if labels[0] == '???':
+    del (labels[0])
+
+# Load the Tensorflow Lite model.
+# If using Edge TPU, use special load_delegate argument
+if use_TPU:
+    interpreter = Interpreter(model_path=PATH_TO_CKPT,
+                              experimental_delegates=[load_delegate('libedgetpu.so.1.0')])
+    print(PATH_TO_CKPT)
+else:
+    interpreter = Interpreter(model_path=PATH_TO_CKPT)
+
+interpreter.allocate_tensors()
+
+# Get model details
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+height = input_details[0]['shape'][1]
+width = input_details[0]['shape'][2]
+
+floating_model = (input_details[0]['dtype'] == np.float32)
+
+input_mean = 127.5
+input_std = 127.5
+
+# Initialize frame rate calculation
+frame_rate_calc = 1
+freq = cv2.getTickFrequency()
+
+# Initialize video stream
+videostream = VideoStream(resolution=(imW, imH), framerate=30).start()
+time.sleep(1)
+
+# for frame1 in camera.capture_continuous(rawCapture, format="bgr",use_video_port=True):
+def get_objects():
+
+    # Start timer (for calculating frame rate)
+    t1 = cv2.getTickCount()
+
+    # Grab frame from video stream
+    frame1 = videostream.read()
+
+    # Acquire frame and resize to expected shape [1xHxWx3]
+    frame = frame1.copy()
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    frame_resized = cv2.resize(frame_rgb, (width, height))
+    input_data = np.expand_dims(frame_resized, axis=0)
+
+    # Normalize pixel values if using a floating model (i.e. if model is non-quantized)
+    if floating_model:
+        input_data = (np.float32(input_data) - input_mean) / input_std
+
+    # Perform the actual detection by running the model with the image as input
+    interpreter.set_tensor(input_details[0]['index'], input_data)
+    interpreter.invoke()
+
+    # Retrieve detection results
+    boxes = interpreter.get_tensor(output_details[0]['index'])[0]  # Bounding box coordinates of detected objects
+    classes = interpreter.get_tensor(output_details[1]['index'])[0]  # Class index of detected objects
+    scores = interpreter.get_tensor(output_details[2]['index'])[0]  # Confidence of detected objects
+    # num = interpreter.get_tensor(output_details[3]['index'])[0]  # Total number of detected objects (inaccurate and not needed)
+    i = np.argmax(scores)
+    if scores[i] > min_conf_threshold:
+        # Get bounding box coordinates and draw box
+        # Interpreter can return coordinates that are outside of image dimensions, need to force them to be within image using max() and min()
+        ymin = int(max(1, (boxes[i][0] * imH)))
+        xmin = int(max(1, (boxes[i][1] * imW)))
+        ymax = int(min(imH, (boxes[i][2] * imH)))
+        xmax = int(min(imW, (boxes[i][3] * imW)))
+
+        bounding_box = (frame, (xmin, ymin), (xmax, ymax), (10, 255, 0), 2)
+
+        # Draw label
+        object_name = labels[int(classes[i])]  # Look up object name from "labels" array using class index
+        label = '%s: %d%%' % (object_name, int(scores[i] * 100))  # Example: 'person: 72%'
+        labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2)  # Get font size
+        label_ymin = max(ymin, labelSize[1] + 10)  # Make sure not to draw label too close to top of window
+
+
+        xcenter = xmin + (int(round((xmax - xmin) / 2)))
+        ycenter = ymin + (int(round((ymax - ymin) / 2)))
+        # Print info
+        print('Object ' + str(i) + ': ' + object_name + ' at (' + str(xcenter) + ', ' + str(ycenter) + ')')
+
+    # All the results have been drawn on the frame, so it's time to display it.
+    # cv2.imshow('Object detector', frame)
+
+    # Calculate framerate
+    t2 = cv2.getTickCount()
+    time1 = (t2 - t1) / freq
+    frame_rate_calc = 1 / time1
+    return object_name, bounding_box
+
 ToF.start_ranging()
 # Running infinite loop to get constant video feeds
 while True:
+    isButtonPressed = my_button.is_button_pressed() is True
     try:
         _, screen = cap.read()
 
@@ -318,11 +540,13 @@ while True:
         write_and_print("Done")
         ToF.stop_ranging()
 
-    cv2.imshow("Navigation View", run(screen))
+    cv2.imshow("Navigation View", run(screen, isButtonPressed))
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
         ToF.stop_ranging()
 
 cap.release()
+# Clean up
 cv2.destroyAllWindows()
+videostream.stop()
---
+++ src/vibrate.py
@@ -0,0 +1,120 @@
+import busio
+import board
+import time
+import numpy as np
+from adafruit_bus_device.i2c_device import I2CDevice
+
+DEVICE_ADDRESS = 0x4a  # device address of the haptic
+
+
+# The follow is for I2C communications
+i2c = busio.I2C(board.SCL, board.SDA)
+device = I2CDevice(i2c, DEVICE_ADDRESS)
+
+def write_register(dev, register, mask, value, n_bytes=1, _startPosition = 0):
+    # Write a wregister number and value
+    init = np.ubyte(0)
+    init = read_register(dev, register, n_bytes)
+    init &= mask
+    init |= (value << _startPosition)
+    buf = bytearray(1 + n_bytes)
+    buf[0] = register
+    buf[1:] = init.tobytes()
+    with dev:
+        dev.write(buf)
+
+def read_register(dev, register, n_bytes=1) -> np.ubyte:
+    # write a register number then read back the value
+    reg = register.to_bytes(1, 'little')
+    buf = bytearray(n_bytes)
+    with dev:
+        dev.write_then_readinto(reg, buf)
+    return np.frombuffer(buf, dtype=np.ubyte)
+
+
+def begin():
+    chipRev = np.ubyte(0)
+    btn_status = read_register(device, 0x00)
+    chipRev |= btn_status << 8
+    chipRev |= btn_status
+    return 0xBA == chipRev[0]
+
+def vibrate(val):
+    accelState = read_register(device, 0x13)[0]
+    accelState &= 0x04
+    accelState = accelState >> 2
+    if accelState == 0x01 and val > 0x7F:
+        val = 0x7F
+    elif val > 0xFF:
+        val = 0xFF
+
+    write_register(device, 0x23, 0x00, val, 7)
+
+def get_vibrate():
+    return read_register(device, 0x23)
+
+if not begin():
+    print('Unable to find motor')
+
+def set_actuator_type():
+    ## LRA_TYPE == 0x00
+    write_register(device, 0x13, 0xDF, 0x00, 5, 5)
+
+def set_actuator_abs_volt(voltage):
+    voltage = voltage / (23.4 * pow(10, -3))
+    write_register(device, 0x0D, 0x00, np.ubyte(voltage), 7)
+
+def set_actuator_nom_volt(voltage):
+    voltage = voltage / (23.4 * pow(10, -3))
+    write_register(device, 0x0C, 0x00, np.ubyte(voltage), 7)
+
+def set_actuator_imax(max_c):
+    max_c = (max_c - 28.6)/7.2
+    write_register(device, 0x0E, 0xE0, np.ubyte(max_c), 4)
+
+def set_actuator_impedence(resistance):
+    max_c = read_register(device, 0x0E) | 0x1F
+    v2iFactor = np.ubyte((resistance * (max_c + 4)) / 1.6104);
+    msbImpedance = (v2iFactor - (v2iFactor & 0x00FF)) / 256
+    lsbImpedance = (v2iFactor - (256 * (v2iFactor & 0x00FF)))
+    write_register(device, 0x10, 0x00, np.ubyte(lsbImpedance), 7)
+    write_register(device, 0x0F, 0x00, np.ubyte(msbImpedance), 7)
+
+def enable_freq_track(enable):
+    write_register(device, 0x13, 0xF7, enable, 1, 3)
+
+def set_actuator_lra_freq(frequency):
+    lraPeriod = np.ubyte(1 / (frequency * (1333.32 * pow(10, -9))))
+    msbFrequency = (lraPeriod - (lraPeriod & 0x007F)) / 128
+    lsbFrequency = (lraPeriod - 128 * (lraPeriod & 0xFF00))
+
+    write_register(device, 0x46, 0x00, np.ubyte(msbFrequency), 7)
+    write_register(device, 0x47, 0x80, np.ubyte(lsbFrequency), 7)
+
+def set_operation_in_dro_mode():
+    ## DRO_MODE == 0x01
+    write_register(device, 0x22, 0xF8, 0x01, 1)
+
+def set_all_defaults():
+    set_actuator_type()
+    set_actuator_abs_volt(2.5)
+    set_actuator_nom_volt(2.5)
+    set_actuator_imax(165.4)
+    set_actuator_impedence(13.8)
+    set_actuator_lra_freq(170)
+    enable_freq_track(False)
+
+set_all_defaults()
+time.sleep(1)
+
+set_operation_in_dro_mode()
+
+while True:
+    try:
+        vibrate(0x64)
+        print(get_vibrate())
+        # don't slam the i2c bus
+        time.sleep(1)
+        vibrate(np.ubyte(0))
+    except KeyboardInterrupt:
+        break
